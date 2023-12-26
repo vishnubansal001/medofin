d7d44a8f3ad47ad498c3b6216093fa7a76563ea4
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-12-26 20:05:29 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import SignIn from "./pages/signIn/SignIn";
+import SignUp from "./pages/signUp/SignUp";
+import Labs from "./pages/labs/Labs";
+
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Routes>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route path="/sign-in" element={<SignIn />}></Route>
+      <Route path="/sign-up" element={<SignUp />}></Route>
+      <Route path="/labs" element={<Labs />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
