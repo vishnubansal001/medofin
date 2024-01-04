4a25cd2912c6b8ae221f3bd946f393116bce3da5
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2024-01-04 17:25:13 +0530

    removed error

--- client/src/components/pharmecy/Slide.jsx
+++ client/src/components/pharmecy/Slide.jsx
@@ -2,7 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { slide } from "../../database/data";
-import { Autoplay } from "swiper/modules";
+import { Autoplay } from "swiper/core";
 
 const Slide = () => {
   return (
