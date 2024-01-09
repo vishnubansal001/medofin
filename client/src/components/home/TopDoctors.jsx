c5e50e91e45d0be8ada5452cbdb44feb355145b2
Sahil <Sahil2003@gmail.com>
2024-01-09 14:21:45 +0530

    Some Ui updations done in top doctors section

--- client/src/components/home/TopDoctors.jsx
+++ client/src/components/home/TopDoctors.jsx
@@ -5,7 +5,7 @@ const TopDoctors = () => {
     return (
         <section className='mb-20'>
             <div className='xl:w-[470px]  mx-auto'>
-                <h2 className=' text-4xl font-semibold text-center'>Our top rated Doctors</h2>
+                <h2 className=' text-4xl font-bold text-center'>Our top rated Doctors</h2>
                 <p className='text-center mt-4'>World-class care for everyone. Our health system offeres unmatched, expert health care.</p>
             </div>
             <DoctorList />
